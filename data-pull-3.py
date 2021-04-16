e11a8d5f6896b2b9b1b9266f633ac46cec6e5cf6
Matt Averill <maverill@ad.busek.com>
2021-04-16 17:18:38 -0400

    added datasets for top 200 cryptos by market cap

--- data-pull-3.py
+++ data-pull-3.py
@@ -1,16 +1,42 @@
 from cryptocmd import CmcScraper
+import shutil
+import csv
 
-# initialise scraper without time interval
-scraper = CmcScraper("STORJ")
+# generate list of top 200 crypto tickers by market cap
+top_200_tickers = []
+with open('top_200.csv', newline='') as csvfile:
+     tickerreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     i = 0
+     for tick in tickerreader:
+         top_200_tickers.append(tick[0])
+         i += 1
+top_200_tickers[0] = 'BTC'
 
-# get raw data as list of list
-headers, data = scraper.get_data()
+j = 0
+for ticker in top_200_tickers:
+    # initialise scraper without time interval
+    scraper = CmcScraper(ticker)
 
-# get data in a json format
-stroj_json_data = scraper.get_data("json")
+    # get raw data as list of list
+    headers, data = scraper.get_data()
 
-# export the data as csv file, you can also pass optional `name` parameter
-scraper.export("csv", name="storj_all_time")
+    # get data in a json format
+    stroj_json_data = scraper.get_data("json")
 
-# Pandas dataFrame for the same data
-df = scraper.get_dataframe()
+    # export the data as csv file, you can also pass optional `name` parameter
+    scraper.export("csv", name=ticker)
+    path_partial1 = 'C:/Users/Matthew/Documents/.crypto/'
+    path_partial2 = 'C:/Users/Matthew/Documents/.crypto/DATA/'
+    ext = '.csv'
+    path_full1 = path_partial1 + ticker + ext
+    path_full2 = path_partial2 + ticker + ext
+    shutil.move(path_full1, path_full2)
+
+    # Pandas dataFrame for the same data
+    df = scraper.get_dataframe()
+
+    # display progress
+    phrase = '/200 Datasets Complete'
+    num_complete = str(j + 1)
+    print(num_complete + phrase)
+    j += 1
