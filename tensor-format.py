933b5816c68015991680b0ed601ed9c933e14a23
Matt Averill <maverill@ad.busek.com>
2021-05-19 17:29:30 -0400

    indexed the opening prices of an asset and converted them to floats

--- tensor-format.py
+++ tensor-format.py
@@ -15,18 +15,24 @@ samples = i
 float_data = np.zeros((samples-1, 200))
 
 #populate each column of the numpy array with single asset
-for i in range(200):
-    with open('top_200.csv', newline='', encoding='utf-8-sig') as top200:
-        file = csv.reader(top200, dialect='excel', delimiter=',', quotechar='|')
-        for row in file:
-            print(i)
-            path1 = 'DATA/'
-            path2 = row[i]
-            path3 = '.csv'
-            path = path1 + path2 + path3
-            print(path)
-            # with open(path, newline='') as asset:
-            #     file = csv.reader(asset, dialect='excel', delimiter='\n', quotechar='|')
-            #     for j in range(samples):
 
-#left off trying to take the first 8 digits of the opening price for each asset and filling in each timestep one asset at a time
+#get the path name for each asset
+with open('top_200.csv', newline='', encoding='utf-8-sig') as top200:
+    file = csv.reader(top200, dialect='excel', delimiter=',', quotechar='|')
+    for row in file:
+        path1 = 'DATA/'
+        path2 = row[0]
+        path3 = '.csv'
+        path = path1 + path2 + path3
+        #index the the opening price for each timestep one asset at a time
+        with open(path, newline='') as asset:
+            file = csv.reader(asset, dialect='excel', delimiter=',', quotechar='|')
+            next(file, None)  # skip the headers
+            for j in file:
+                num = float(j[1])
+                print(type(num))
+            break
+            
+
+#left off converting each opening price to floats one asset at a time, not one timestep at a time
+#need to manipulate every asset but BTC to have zeros in the timesteps that don't have data
