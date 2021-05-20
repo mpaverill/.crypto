9df798343bab58a8f52ca40b85660714e9efe18a
Matt Averill <maverill@ad.busek.com>
2021-05-20 17:14:57 -0400

    WIP adding zeros to missing data of each assset

--- tensor-format.py
+++ tensor-format.py
@@ -1,6 +1,13 @@
 import numpy as np
 import csv
 
+def add_zeros(file_name, num_rows):
+        with open(file_name, 'a+', newline='') as asset:
+        # Create a writer object from csv module
+        csv_writer = writer(asset)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(
+
 #determine the # of timesteps in timeseries
 with open('DATA/BTC.csv', newline='', encoding='utf-8-sig') as btc:
     file = csv.reader(btc, dialect='excel', delimiter='\n', quotechar='|')
@@ -10,6 +17,7 @@ with open('DATA/BTC.csv', newline='', encoding='utf-8-sig') as btc:
             headers = row[i]
         i += 1  
 samples = i
+print(samples-1)
 
 #construct empty timeseries tensor
 float_data = np.zeros((samples-1, 200))
@@ -24,15 +32,17 @@ with open('top_200.csv', newline='', encoding='utf-8-sig') as top200:
         path2 = row[0]
         path3 = '.csv'
         path = path1 + path2 + path3
+
+        #add zeros to missing data in each asset
+        max_rows = samples - 1
+
         #index the the opening price for each timestep one asset at a time
         with open(path, newline='') as asset:
             file = csv.reader(asset, dialect='excel', delimiter=',', quotechar='|')
             next(file, None)  # skip the headers
             for j in file:
                 num = float(j[1])
-                print(type(num))
             break
             
 
-#left off converting each opening price to floats one asset at a time, not one timestep at a time
-#need to manipulate every asset but BTC to have zeros in the timesteps that don't have data
+#left off creating a function to add zeros to the missing data of each asset
