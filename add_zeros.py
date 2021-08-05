5f3a690e73cfaa5a832c86060b54c8a99bc63c71
Matt Averill <maverill@ad.busek.com>
2021-08-05 16:58:36 -0400

    Successfully added zeroes to a test csv file for missing data

--- add_zeros.py
+++ add_zeros.py
@@ -5,16 +5,31 @@ import numpy as np
 
 def add_zeros(file_name, num_rows, max_rows):
     miss_rows = max_rows - num_rows
-    names = ['Time', 'Open', 'Close', 'High', 'Low', 'Volume']
-    df = pd.DataFrame(np.zeros((miss_rows, 6)), dtype=float, columns=names)
+    names = ['time', 'open']
+    df1 = pd.DataFrame(np.zeros((miss_rows, 2)), dtype=float, columns=names)
+    
+    df2 = pd.read_csv('DATA_BITFINEX/test.csv')
+    del df2['Unnamed: 0']
+    del df2['volume']
+    del df2['low']
+    del df2['high']
+    del df2['close']
 
-    with open(file_name, newline='', encoding='utf-8-sig') as file:
-        file = csv.reader(file, dialect='excel', delimiter=',', quotechar='|')
-        next(file, None)  #skip the headers
-        for row in file: #iterates through each ASSET
-            df.append(row)
-    print(df)
+    df3 = df1.append(df2)
+    print(df3)
 
+#LEFT OFF APPENDING ASSET DF TO ZEROS DF. THE RESULTING DF WILL CONVERTED TO CSV AND THEN PLACED INTO THE TENSOR
+
+
+
+    # with open(file_name, newline='', encoding='utf-8-sig') as file:
+    #     file = csv.reader(file, dialect='excel', delimiter=',', quotechar='|')
+    #     next(file, None)  #skip the headers
+    #     for row in file: #iterates through each ASSET
+    #         for value in row:
+    #             value = float(value)
+    #             df.append(value)
+            
 
     # new_row = [0.0,0.0]
     # with open(file_name, 'a+', newline='', encoding='utf-8-sig') as asset:
@@ -24,4 +39,4 @@ def add_zeros(file_name, num_rows, max_rows):
     #     for i in range(num_rows):
     #         csv_writer.writerow(new_row)
 
-add_zeros(file_name='DATA_BITFINEX/dogeusd.csv', max_rows = 3420458, num_rows = 900000)
+add_zeros(file_name='DATA_BITFINEX/test.csv', max_rows = 45, num_rows = 30)
